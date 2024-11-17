683853d7ccf602188d38cd9297cc420080db08a6
HASNAOUI <162112576+hasnaoui02@users.noreply.github.com>
2024-11-17 14:48:51 +0100

    Update product.py

--- addons/gestionVehicules/models/product.py
+++ addons/gestionVehicules/models/product.py
@@ -1,8 +1,8 @@
 from odoo import models, fields
 
 class Product(models.Model):
-    _inherit = ['product.product']  # Inherit the product model
+    _inherit = 'product.product'# Inherit the existing product model
 
-    quantity_used = fields.Integer(string='Quantity Used')
-    usage_date = fields.Date(string='Usage Date')
-    project_id = fields.Many2one('construction.project', string='Associated Project')
+    quantity_used = fields.Integer(string='Quantity Used')  # New field for quantity used
+    usage_date = fields.Date(string='Usage Date')  # New field for usage date
+    project_id = fields.Many2one('construction.project', string='Associated Project')  # Field to link to construction.project
