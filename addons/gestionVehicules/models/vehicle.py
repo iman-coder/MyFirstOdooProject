ff3d59210bc145c984563975bccf879fdc8df912
HASNAOUI <162112576+hasnaoui02@users.noreply.github.com>
2024-11-17 14:51:32 +0100

    Update vehicle.py

--- addons/gestionVehicules/models/vehicle.py
+++ addons/gestionVehicules/models/vehicle.py
@@ -1,13 +1,6 @@
 from odoo import api, fields, models
 
-class Vehicle(models.Model):
-    _inherit = ['fleet.vehicle']  # Inherit the vehicle model
-    
-    immatricule = fields.Char('Immatricule ')
-    daily_cost = fields.Float(string='Daily Cost')
-    usage_date = fields.Date(string='Usage Date')
-    project_id = fields.Many2one('construction.project', string='Assigned Project')
-'''
+
 class vehicule(models.Model):
     _name = "vehicule"
     _inherit = ['fleet.vehicle']
@@ -18,5 +11,5 @@ class vehicule(models.Model):
     type= fields.Selection([('Voiture','Voiture'),('Moto','Moto'),('Camion','Camion'),('Tracteur','Tracteur'),('Velo','Velo')])
     etat= fields.Selection([('Disponible','Disponible'),('En mission','En mission'),('En reparation','En reparation'),('En panne','En panne')])
     photo = fields.Binary(string="Photo", attachment=True)
-'''
+
     
